9748276fdd5af59a3176949b7c55ed3a1ac5201c
Kesha <kesha.tkachenko2017@mail.ru>
2020-10-17 17:57:15 +0300

    added bug

--- GHActTest2/Program.cs
+++ GHActTest2/Program.cs
@@ -20,7 +20,7 @@ namespace GHActTest2
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startu>();
                 });
     }
 }
